c4675f7944794df32e8b80d5940d48cc60778ff0
Isac Souza Filho <souzafilhoisac@gmail.com>
2022-12-13 13:54:50 -0300

    conceitos de injeção de dependência

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MailService } from './mail/mail.service';
+import { PostmarkMailService } from './mail/postmark-mail.service';
+import { SMTPMailService } from './mail/smtp-mail.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    {
+      provide: MailService,
+      useClass: PostmarkMailService,
+    },
+  ],
 })
 export class AppModule {}
