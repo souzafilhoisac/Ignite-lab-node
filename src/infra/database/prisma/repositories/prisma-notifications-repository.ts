f9036ad280599f51fbc170f0ddbfb1fe004ffe09
Isac Souza Filho <souzafilhoisac@gmail.com>
2022-12-15 21:52:06 -0300

    cancelamento de notificações e errors not found

--- src/infra/database/prisma/repositories/prisma-notifications-repository.ts
+++ src/infra/database/prisma/repositories/prisma-notifications-repository.ts
@@ -1,12 +1,16 @@
-import { Injectable } from '@nestjs/common';
 import { Notification } from '@app/entities/notification';
 import { NotificationsRepository } from '@app/repositories/notification-repository';
-import { PrismaService } from '../prisma.service';
+import { Injectable } from '@nestjs/common';
 import { PrismaNotificationMapper } from '../mappers/prisma-notification-mapper';
+import { PrismaService } from '../prisma.service';
 
 @Injectable()
 export class PrismaNotificationsRepository implements NotificationsRepository {
   constructor(private prismaService: PrismaService) {}
+
+  async findById(notificationId: string): Promise<Notification | null> {
+    throw new Error('Method not implemented.');
+  }
 
   async create(notification: Notification): Promise<void> {
     const raw = PrismaNotificationMapper.toPrisma(notification);
@@ -14,5 +18,9 @@ export class PrismaNotificationsRepository implements NotificationsRepository {
     await this.prismaService.notification.create({
       data: raw,
     });
+  }
+
+  async save(notification: Notification): Promise<void> {
+    throw new Error('Method not implemented.');
   }
 }
